b1fca37e48188bc68109c43138cd3b583d86dc2a
unknown <leo.moleiromurcio@gmail.com>
2023-02-27 22:21:59 -0300

    Meu Nome

---
+++ leo.py
@@ -0,0 +1,7 @@
+nome = "Leo"
+sobrenome = " Moleiro Murcio"
+idade = "20"
+altura = "1.75"
+peso = "70kg"
+
+print ("Meu nome é " + nome + sobrenome + ", tenho " +idade+ " anos e tenho " +altura+ " de altura ")
